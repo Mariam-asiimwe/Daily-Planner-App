4c98ec7163062ba2780a0991cfefc26b922b32c3
Mariam-asiimwe <m.asiimwe@outlook.com>
2023-02-01 17:53:47 +0000

    added code to allow me to submit assignment

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ let currentDay = moment("currentDay", "dddd Do MMMM");
 $("#currentDay").text(moment().format("dddd Do MMMM"));
 
 //Display timeblocks for standard business hours when the user scrolls down
-$("time").text(moment().format("HH:mm"));
+//$("time").text(moment().format("HH:mm"));
 //hourly interval
-setInterval(
-  function () {
-    $("#currentTime").text(moment().format("DDD MMM, YYYY hh:mm:ss"))
+// localStorage.setItem("9 AM", "data-item");
+// $("textarea").addClass("future")
 
-  }, 1000)
+// setInterval(
+//   function () {
+//     $("#currentTime").text(moment().format("DDD MMM, YYYY hh:mm:ss"))
+
+//   }, 1000)
+
 //$("<p>").textContent("",);
-/*let buttonListEl = $(".button");
+let buttonListEl = $(".button");
+
+//data-time
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -30,24 +36,24 @@ function handleFormSubmit(event) {
   activityEl.text(activity);
 }
 
-  event.preventDefault();
-  $("#9 AM").append(
-    <div class="row">
-      <div class="col">
-        current time
-        <p id="text">9AM</p>
-      </div>
-      <div class="col-6">
-        2 of 3 (wider)
-        <p id="text"></p>
-      </div>
-      <div class="col">
-        3 of 3
-        <p id="text"></p>
-      </div>
-    </div>
-  )
-})*/
+//   event.preventDefault();
+//   $("#9 AM").append(
+//     <div class="row">
+//       <div class="col">
+//         current time
+//         <p id="text">9AM</p>
+//       </div>
+//       <div class="col-6">
+//         2 of 3 (wider)
+//         <p id="text"></p>
+//       </div>
+//       <div class="col">
+//         3 of 3
+//         <p id="text"></p>
+//       </div>
+//     </div>
+//   )
+// });
 
 
 //color change interval 
@@ -55,7 +61,7 @@ const myInterval = setInterval(setColor, 3600000);
 
 function setColor() {
   let x = document.body;
-  x.style.backgroundColor = x.style.backgroundColor == "grey" ? "blue" : "green";
+  x.style.backgroundColor = x.style.backgroundColor == "grey", "blue" , "green";
 }
 
 function stopColor() {
